59bbaf495f9d951242baf04dbb8c37b3a8032086
Stephan <42045699+stephanrh@users.noreply.github.com>
2019-07-10 19:57:21 -0500

    A-D math worked out

--- kinetic.py
+++ kinetic.py
@@ -22,6 +22,7 @@ from __future__ import division
 import numpy as np
 import xarray as xr
 import matplotlib.pyplot as plt
+import pylab
 
 
 ###############################################################################
@@ -48,20 +49,20 @@ number_of_samples = 100
 
 ki_low = 9e-5 #k_{01}
 kii_low = 9e-6 #k_{12}
-kiii_low = 9e-4 #k_{23}
-#kiiii_low = 1e-8 #K_{34}
+kiii_low = 9e-6 #k_{23}
+kiiii_low = 2e-5 #K_{34}
 
 ki_hi = 2e-4
 kii_hi = 2e-5
-kiii_hi = 2e-3
-#kiiii_hi = 5e-7
+kiii_hi = 2e-5
+kiiii_hi = 5e-5
 
 
 
 ki_range = np.linspace(ki_low,ki_hi,10)
 kii_range = np.linspace(kii_low,kii_hi,10)
 kiii_range = np.linspace(kiii_low,kiii_hi,10)
-#kiiii_range = np.linspace(kiiii_low,kiiii_hi,1000)
+kiiii_range = np.linspace(kiiii_low,kiiii_hi,10)
 
 #note: no kv for E needed
 #note: ki>kii>kiii>kiiii
@@ -99,14 +100,14 @@ rand_kii = np.array([np.random.choice(10) for i in range(number_of_samples)])
 np.random.seed(2)
 rand_kiii = np.array([np.random.choice(10) for i in range(number_of_samples)])
 
-#np.random.seed(3)
-#rand_kiiii = np.array([np.random.choice(1000) for i in xrange(number_of_samples)])
+np.random.seed(3)
+rand_kiiii = np.array([np.random.choice(10) for i in range(number_of_samples)])
 
 
 ki_randomized = ki_range[rand_ki]
 kii_randomized = kii_range[rand_kii]
 kiii_randomized = kiii_range[rand_kiii]
-#kiiii_randomized = kiiii_range[rand_kiiii]
+kiiii_randomized = kiiii_range[rand_kiiii]
 
 
 ###############################################################################
@@ -121,7 +122,7 @@ C_solutions = np.zeros((number_of_samples,7))
 D_solutions = np.zeros((number_of_samples,7))
 E_solutions = np.zeros((number_of_samples,7))
 
-for indexer, (ki, kii, kiii) in enumerate(zip(ki_randomized, kii_randomized, kiii_randomized)):
+for indexer, (ki, kii, kiii, kiiii) in enumerate(zip(ki_randomized, kii_randomized, kiii_randomized, kiiii_randomized)):
 
     for sec_indexer, (t, A_o) in enumerate(zip(time_values, Ao_realvalues)):
 
@@ -134,19 +135,44 @@ for indexer, (ki, kii, kiii) in enumerate(zip(ki_randomized, kii_randomized, kii
 
         #Subject to change, Natalia and Dimtry will check my math for these equations
 
-        C = np.divide((ki*A_o),(kii-ki)) * (((kiii-ki) * np.exp(-ki*t)) - ((kiii-kii) * np.exp(-kii*t)) + ((ki-kii) * np.exp(-kiii*t)))
+        C = (np.divide((kii*ki*A_o),(kii-ki)) * \
+                    ((np.divide(1,(kiii-ki)) * np.exp(-ki*t)) - \
+                    (np.divide(1,(kiii-kii)) * (np.exp(-kii*t))) + \
+                    (np.divide(1 ,(kiii-kii)) * (np.exp(-kiii*t))) - \
+                    (np.divide(1 ,(kiii-ki)) * (np.exp(-kiii*t)))))
         #MARIA'S COMMENT: Please double check bracket placements in equation for D! -- SRH they are correct
 
-        D = np.divide((ki*A_o*kiii), (kii-ki)) * \
-                      ((np.divide((kiii-ki),(-ki)) * np.exp(-ki*t)) - \
-                       (np.divide((kiii-kii),(-kii)) * np.exp(-kii*t)) + \
-                       (np.divide((ki-kii),(-kiii)) * np.exp(-kiii*t))) - \
-                       (np.divide((ki*A_o*kiii), (kii-ki)) * \
-                        (np.divide((kiii-ki),(-ki)) - \
-                         np.divide((kiii-kii),(-kii)) + \
-                         np.divide((ki-kii),(-kiii))))
+        D = np.divide((kiii*kii*ki*A_o), (kii-ki))  * \
+                      ((np.divide(1, ((kiii-ki)*(kiiii-ki))) * np.exp(-ki*t)) - \
+                       (np.divide(1, ((kiii-kii)*(kiiii-kii))) * np.exp(-kii*t)) + \
+                       (np.divide(1, ((kiii-kii)*(kiiii-kiii))) * np.exp(-kiii*t)) - \
+                       (np.divide(1, ((kiii-ki)*(kiiii-kiii))) * np.exp(-kiii*t)) - \
+                       (np.divide(1, ((kiii-ki)*(kiiii-ki))) * np.exp(-kiiii*t)) + \
+                       (np.divide(1, ((kiii-kii)*(kiiii-kii))) * np.exp(-kiiii*t)) - \
+                       (np.divide(1, ((kiii-kii)*(kiiii-kiii))) * np.exp(-kiiii*t)) + \
+                       (np.divide(1, ((kiii-ki)*(kiiii-kiii))) * np.exp(-kiiii*t)))
 
-        E = A_o - A - B - C - D #Testing with E to see where an error might be in the equations
+        E = A_o - A - B - C - D
+
+        '''
+        E = np.divide((kiiii*kiii*kii*ki*A_o), (kii-ki))  * \
+                      ((np.divide(1, ((kiii-ki)*(kiiii-ki)*(-ki))) * np.exp(-ki*t)) - \
+                       (np.divide(1, ((kiii-kii)*(kiiii-kii)*(-kii))) * np.exp(-kii*t)) + \
+                       (np.divide(1, ((kiii-kii)*(kiiii-kiii)*(-kiii))) * np.exp(-kiii*t)) - \
+                       (np.divide(1, ((kiii-ki)*(kiiii-kiii)*(-kiii))) * np.exp(-kiii*t)) - \
+                       (np.divide(1, ((kiii-ki)*(kiiii-ki)*(-kiiii))) * np.exp(-kiiii*t)) + \
+                       (np.divide(1, ((kiii-kii)*(kiiii-kii)*(-kiiii))) * np.exp(-kiiii*t)) - \
+                       (np.divide(1, ((kiii-kii)*(kiiii-kiii)*(-kiiii))) * np.exp(-kiiii*t)) + \
+                       (np.divide(1, ((kiii-ki)*(kiiii-kiii)*(-kiiii))) * np.exp(-kiiii*t)) - \
+                       np.divide(1, ((kiii-ki)*(kiiii-ki)*(-ki))) + \
+                       np.divide(1, ((kiii-kii)*(kiiii-kii)*(-kii))) - \
+                       np.divide(1, ((kiii-kii)*(kiiii-kiii)*(-kiii))) + \
+                       np.divide(1, ((kiii-ki)*(kiiii-kiii)*(-kiii))) + \
+                       np.divide(1, ((kiii-ki)*(kiiii-ki)*(-kiiii))) - \
+                       np.divide(1, ((kiii-kii)*(kiiii-kii)*(-kiiii))) + \
+                       np.divide(1, ((kiii-kii)*(kiiii-kiii)*(-kiiii))) - \
+                       np.divide(1, ((kiii-kii)*(kiiii-kiii)*(-kiiii))))
+        '''
 
         A_solutions[indexer, sec_indexer] = A
         B_solutions[indexer, sec_indexer] = B
@@ -193,18 +219,18 @@ for i in range(0,number_of_samples):
     #print i
 
     #the line plots, time versus data
-    ls2, = ax.plot(time_values, A_solutions[i,:], c='grey', zorder=5)
-    ls3, = ax.plot(time_values, B_solutions[i,:], c='goldenrod', zorder=4)
-    ls4, = ax.plot(time_values, C_solutions[i,:], c='chocolate', zorder=3)
-    ls5, = ax.plot(time_values, D_solutions[i,:], c='royalblue', zorder=2)
-    #ls6, = ax.plot(time_values, E_solutions[i,:], c='dodgerblue', zorder=1)
+    ls2, = ax.plot(time_values, A_solutions[i,:], c='grey', zorder=5, alpha=0.3)
+    ls3, = ax.plot(time_values, B_solutions[i,:], c='goldenrod', zorder=4, alpha=0.3)
+    ls4, = ax.plot(time_values, C_solutions[i,:], c='chocolate', zorder=3, alpha=0.3)
+    ls5, = ax.plot(time_values, D_solutions[i,:], c='royalblue', zorder=2, alpha=0.3)
+    ls6, = ax.plot(time_values, E_solutions[i,:], c='dodgerblue', zorder=1, alpha=0.3)
 
     #the scatter points of data observations
-    ax.scatter(time_values, A_solutions[i,:], c='grey', zorder=5)
-    ax.scatter(time_values, B_solutions[i,:], c='goldenrod', zorder=4)
-    ax.scatter(time_values, C_solutions[i,:], c='chocolate', zorder=3)
-    ax.scatter(time_values, D_solutions[i,:], c='royalblue', zorder=2)
-    #ax.scatter(time_values, E_solutions[i,:], c='dodgerblue', zorder=1)
+    ax.scatter(time_values, A_solutions[i,:], c='grey', zorder=5, alpha=0.1)
+    ax.scatter(time_values, B_solutions[i,:], c='goldenrod', zorder=4, alpha=0.3)
+    ax.scatter(time_values, C_solutions[i,:], c='chocolate', zorder=3, alpha=0.3)
+    ax.scatter(time_values, D_solutions[i,:], c='royalblue', zorder=2, alpha=0.3)
+    ax.scatter(time_values, E_solutions[i,:], c='dodgerblue', zorder=1,  alpha=0.3)
 
 #force plot to not have any buffer space
 plt.margins(x=0, y=0)
@@ -213,10 +239,16 @@ plt.margins(x=0, y=0)
 ax.set_xlabel(r'Time (s; $ \times 10^{5}$)', fontsize=12, color='k')
 ax.set_ylabel(r'[Mo] ($\mu$m; $ \times 10^{-5}$)', fontsize=12, color='k')
 
-ax.legend([ls1, ls2, ls3, ls4, ls5, ls6],
+line1 = pylab.Line2D(range(10),range(10),marker="_",linewidth=2.0,color="grey",alpha=1.0)
+line2 = pylab.Line2D(range(10),range(10),marker="_",linewidth=2.0,color="goldenrod",alpha=1.0)
+line3 = pylab.Line2D(range(10),range(10),marker="_",linewidth=2.0,color="chocolate",alpha=1.0)
+line4 = pylab.Line2D(range(10),range(10),marker="_",linewidth=2.0,color="royalblue",alpha=1.0)
+line5 = pylab.Line2D(range(10),range(10),marker="_",linewidth=2.0,color="dodgerblue",alpha=1.0)
+
+ax.legend([ls1, line1, line2, line3, line4, line5],
           ['Mo$_{tot}$','MoO$_{4}$','MoO$_{3}$S','MoO$_{2}$S$_{2}$','MoOS$_{3}$', 'MoS$_{4}$'],
-                              loc='upper right',shadow=True,
-          fancybox=True, ncol=2, fontsize=12, framealpha=1.)
+                              loc='upper right',
+          fancybox=True, ncol=2, fontsize=12, framealpha=0.9)
 
 ax.set_xticks([0.,100000,200000,300000,400000])
 ax.set_xticklabels(['0','1','2','3','4'], fontsize=12)
